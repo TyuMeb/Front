d24ce221865cdb13778d775023495eb5e5356ce0
pipipi666 <nnm107072@gmail.com>
2023-05-09 22:58:13 +0300

    feat: add routes consts

---
+++ src/utils/consts/routes.ts
@@ -0,0 +1,10 @@
+const ROUTES = {
+    HOME: "/",
+    LOGIN: "/login",
+    REGISTER: "/register",
+    FORGOT_PASSWORD: "/forgot-password",
+    RESET_PASSWORD: "/reset-password",
+    PROFILE: "/profile",
+};
+
+export { ROUTES };
