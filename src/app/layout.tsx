f22c96ce52af047bc87121a54f9624a12dd6afa2
pipipi666 <nnm107072@gmail.com>
2023-05-22 13:03:47 +0300

    feat: banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import styles from "./layout.module.css";
 
-const inter = Open_Sans({ subsets: ["latin"] });
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Marketplace Why We",
@@ -14,10 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body className={openSans.className}>
                 <Header />
                 <main className={styles.main}>{children}</main>
-                <Footer />
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import styles from "./page.module.css";
+import clsx from "clsx";
+import { Open_Sans, Raleway } from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <main className={styles.main}>
-            <h1>Hello Next JS</h1>
+            <div className={styles.banner}>
+                <div className={styles.box}>
+                    <div>
+                        <h1 className={clsx(raleway.className, "h1")}>Создайте свой идеальный дом</h1>
+                        <p className={clsx(openSans.className, styles.p)}>
+                            Сделайте заказ прямо сейчас и мы подберём вам лучшие предложения за 24 часа
+                        </p>
+                        <button>Сделать заказ</button>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
