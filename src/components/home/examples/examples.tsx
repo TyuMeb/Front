cb59ead90519f512020295a7451256f7e4f1ca2f
Arkady Schapov <sigma696@gmail.com>
2023-07-22 19:20:56 +0300

    create slider for main page

--- src/components/home/examples/examples.tsx
+++ src/components/home/examples/examples.tsx
@@ -1,21 +1,38 @@
 "use client";
+
 import Image from "next/image";
-
 import { CardExample } from "@src/components/shared/ui/card-example/card-example";
-
 import styles from "./examples.module.scss";
+import cn from "classnames";
+import { useState } from "react";
 
 export const Examples = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    function forwardSlide() {
+        setCurrentSlide((current) => {
+            if (current < 1) return current + 1;
+            else return 0;
+        });
+    }
+
+    function backwardSlide() {
+        setCurrentSlide((current) => {
+            if (current > 0) return current - 1;
+            else return 1;
+        });
+    }
+
     return (
         <div className={styles.inner}>
-            <h2 className={`title-h2 ${styles.title}`}>Примеры работ</h2>
+            <h2 className={cn("title-h2", styles.title)}>Примеры работ</h2>
             <div className={styles.examples}>
                 <div className={styles.leftSide}>
                     <div className={styles.top}>
                         <CardExample
                             width={388}
                             height={208}
-                            src="/home/nightstand.jpg"
+                            src={`/home/slide${currentSlide}0.jpg`}
                             alt="logo-nightstand"
                             object="Полка"
                             price="1000 р"
@@ -23,7 +40,7 @@ export const Examples = () => {
                         <CardExample
                             width={388}
                             height={208}
-                            src="/home/closet-sm.jpg"
+                            src={`/home/slide${currentSlide}1.jpg`}
                             alt="logo-closet-sm"
                             object="Полка"
                             price="1000 р"
@@ -32,7 +49,7 @@ export const Examples = () => {
                     <CardExample
                         width={804}
                         height={326}
-                        src="/home/kitchen.jpg"
+                        src={`/home/slide${currentSlide}2.jpg`}
                         alt="logo-kitchen"
                         object="Полка"
                         price="1000 р"
@@ -42,18 +59,18 @@ export const Examples = () => {
                     <CardExample
                         width={388}
                         height={444}
-                        src="/home/closet-lg.jpg"
+                        src={`/home/slide${currentSlide}3.jpg`}
                         alt="logo-closet-lg"
                         object="Полка"
                         price="1000 р"
                     />
                     <div className={styles.slider}>
-                        <a href="#">
+                        <button className={styles.button} onClick={backwardSlide}>
                             <Image src="/home/arrow-left.svg" alt="arrow-left" width={14} height={32} />
-                        </a>
-                        <a href="#">
+                        </button>
+                        <button className={styles.button} onClick={forwardSlide}>
                             <Image src="/home/arrow-right.svg" alt="arrow-right" width={14} height={32} />
-                        </a>
+                        </button>
                     </div>
                 </div>
             </div>
