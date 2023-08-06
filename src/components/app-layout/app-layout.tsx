ea44f56c00c832dafb8016e0355065141eaf61a0
Resaw <resavv@mail.ru>
2023-08-06 22:39:44 +0300

    delete metadata from app-layout.tsx

--- src/components/app-layout/app-layout.tsx
+++ src/components/app-layout/app-layout.tsx
@@ -1,14 +1,8 @@
 import React, { ReactNode } from "react";
-import { Metadata } from "next";
 import Header from "@src/components/header/header";
 import Footer from "@src/components/footer/footer";
 import { Providers } from "@src/redux/provider";
 import Modals from "@src/components/modals/modals";
-
-export const metadata: Metadata = {
-    title: "Marketplace Why We",
-    description: "Marketplace Why We",
-};
 
 export default function AppLayout({ children }: { children: ReactNode }) {
     return (
