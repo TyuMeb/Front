50d3bf2ca7b0d1baad60b676b50764f6c4af220f
Долгушин Антон <antodolg@gmail.com>
2023-08-10 19:00:44 +0800

    fixed background with Image/NextJS

--- src/components/home/head-block/head-block.tsx
+++ src/components/home/head-block/head-block.tsx
@@ -1,21 +1,40 @@
 "use client";
 
 import styles from "./head-block.module.scss";
+import Image from "next/image";
+import banner from "../../../../public/home/home-banner.jpg";
 
 import { Button } from "@src/components/shared/ui/button/button";
 
 export const HeadBlock = () => {
     return (
-        <section id="banner">
-            <div className={styles.banner}>
-                <div className="container">
-                    <h1 className={styles.title}>Создайте свой идеальный дом</h1>
-                    <p className={styles.description}>
-                        Сделайте заказ прямо сейчас, и мы подберём вам лучшие предложения за 24 часа
-                    </p>
-                    <Button onClick={() => {}}>Сделать заказ</Button>
+        <>
+            <div>
+                <div className={styles.banner}>
+                    <div>
+                        <Image
+                            alt="banner"
+                            src={banner}
+                            placeholder="blur"
+                            quality={100}
+                            style={{
+                                objectFit: "cover",
+                                position: "absolute",
+                                width: "100vw",
+                                height: "554px",
+                                zIndex: -1,
+                            }}
+                        />
+                    </div>
+                    <div className="container">
+                        <h1 className={styles.title}>Создайте свой идеальный дом</h1>
+                        <p className={styles.description}>
+                            Сделайте заказ прямо сейчас, и мы подберём вам лучшие предложения за 24 часа
+                        </p>
+                        <Button onClick={() => {}}>Сделать заказ</Button>
+                    </div>
                 </div>
             </div>
-        </section>
+        </>
     );
 };
