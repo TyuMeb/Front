d23cdd88a07b973a46b66057db4d5503969cdee3
Долгушин Антон <antodolg@gmail.com>
2023-09-19 22:27:44 +0800

    fixed nav

--- src/components/account/nav-account/nav-account.tsx
+++ src/components/account/nav-account/nav-account.tsx
@@ -6,6 +6,11 @@ import styles from "./nav-account.module.scss";
 // import { useAppSelector } from "@src/redux/hooks";
 import { ExitButton } from "@src/components/shared/ui/button/exit-button";
 import { CouchIcon } from "@src/components/shared/ui/icons/couch-icon/couch-icon";
+import { ChatsIcon } from "@src/components/shared/ui/icons/chats-icon/chats-icon";
+import { ClockIcon } from "@src/components/shared/ui/icons/clock-icon/clock-icon";
+import { GearIcon } from "@src/components/shared/ui/icons/gear-icon/gear-icon";
+import { HelpIcon } from "@src/components/shared/ui/icons/help-icon/help-icon";
+import { PlusIcon } from "@src/components/shared/ui/icons/plus-icon/plus-icon";
 
 const NavAccount = () => {
     const [isMessage, setIsMessage] = useState(true);
@@ -64,97 +69,47 @@ const NavAccount = () => {
                                 </a>
                             </li>
                         </ul>
-                        <li className={styles.menuitem}>
-                            <a className={styles.chats} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Чаты
-                            </a>
+                        <li
+                            className={current === "chats" ? styles.active : styles.menuitem}
+                            onClick={() => switchTab("chats")}>
+                            <div className={styles.menuicon}>
+                                <ChatsIcon />
+                            </div>
+                            Чаты
                         </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.archives} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Архивы
-                            </a>
+                        <li
+                            className={current === "clock" ? styles.active : styles.menuitem}
+                            onClick={() => switchTab("clock")}>
+                            <div className={styles.menuicon}>
+                                <ClockIcon />
+                            </div>
+                            Архивы
                         </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.settings} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Настройки
-                            </a>
+                        <li
+                            className={current === "gear" ? styles.active : styles.menuitem}
+                            onClick={() => switchTab("gear")}>
+                            <div className={styles.menuicon}>
+                                <GearIcon />
+                            </div>
+                            Настройки
                         </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.help} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Помощь
-                            </a>
+                        <li
+                            className={current === "help" ? styles.active : styles.menuitem}
+                            onClick={() => switchTab("help")}>
+                            <div className={styles.menuicon}>
+                                <HelpIcon />
+                            </div>
+                            Помощь
                         </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.makeorder} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Сделать заказ
-                            </a>
+                        <li
+                            className={current === "plus" ? styles.active : styles.menuitem}
+                            onClick={() => switchTab("plus")}>
+                            <div className={styles.menuicon}>
+                                <PlusIcon />
+                            </div>
+                            Сделать заказ
                         </li>
                     </ul>
-                    {/* <li className={styles.shelforder}>
-                        <span></span>
-                        <a className={styles.orderlink} href="">
-                            Полка настенная (1)
-                        </a>
-                    </li> */}
-                    {/* <ul className={styles.producer}>
-                        <li className={styles.order}>
-                            <a className={styles.master} href="">
-                                Исполнитель 1
-                            </a>
-                        </li>
-
-                        <li className={styles.order}>
-                            <a className={styles.master} href="">
-                                Исполнитель 2
-                            </a>
-                            {isMessage && <svg xmlns="http://www.w3.org/2000/svg" width="8" height="9" viewBox="0 0 8 9" fill="none">
-                                <circle cx="4" cy="4.5" r="4" fill="#719D88" />
-                            </svg>}
-                        </li>
-                    </ul> */}
-                    {/* <li className={styles.order}>
-                        <span></span>
-                        <a className={styles.orderlink} href="">
-                            Комод
-                        </a>
-                    </li> */}
-                    {/* <ul className={styles.menu}>
-                        <li className={styles.menuitem}>
-                            <a className={styles.chats} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Чаты
-                            </a>
-                        </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.archives} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Архивы
-                            </a>
-                        </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.settings} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Настройки
-                            </a>
-                        </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.help} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Помощь
-                            </a>
-                        </li>
-                        <li className={styles.menuitem}>
-                            <a className={styles.makeorder} href=""></a>
-                            <a className={styles.menulink} href="">
-                                Сделать заказ
-                            </a>
-                        </li>
-                    </ul> */}
                 </nav>
                 <ExitButton onClick={() => {}}>Выйти</ExitButton>
             </div>
@@ -163,60 +118,3 @@ const NavAccount = () => {
 };
 
 export default NavAccount;
-
-// const NavAccount = () => {
-//     const [current, setCurrent] = useState<string | undefined>(undefined);
-//     const { contentBlock } = useAppSelector((store) => store.nav);
-
-//     useEffect(() => {
-//         setCurrent(contentBlock);
-//     }, [contentBlock]);
-
-//     const switchTab = (tab: string) => {
-//         setCurrent(tab);
-//         const element: HTMLElement | null = document.getElementById(tab);
-//         if (element) {
-//             element.id === "form"
-//                 ? element.scrollIntoView({ behavior: "smooth", block: "center" })
-//                 : element.scrollIntoView({ behavior: "smooth", block: "start" });
-//         }
-//     };
-
-//     return (
-//         <nav className={styles.nav}>
-//             <ul className={styles.list}>
-//                 <li
-//                     className={current !== "myorders" ? styles.myorders : styles.active}
-//                     onClick={() => switchTab("myorders")}>
-//                     Мои заказы
-//                 </li>
-//                 <li className={current !== "chats" ? styles.chats : styles.active} onClick={() => switchTab("chats")}>
-//                     Чаты
-//                 </li>
-//                 <li
-//                     className={current !== "archives" ? styles.archives : styles.active}
-//                     onClick={() => switchTab("archives")}>
-//                     Архивы
-//                 </li>
-//                 <li
-//                     className={current !== "settings" ? styles.settings : styles.active}
-//                     onClick={() => switchTab("settings")}>
-//                     Настройки
-//                 </li>
-//                 <li className={current !== "help" ? styles.help : styles.active} onClick={() => switchTab("help")}>
-//                     Помощь
-//                 </li>
-//                 <li
-//                     className={current !== "makeorder" ? styles.makeorder : styles.active}
-//                     onClick={() => switchTab("makeorder")}>
-//                     Сделать заказ
-//                 </li>
-//             </ul>
-//             <div className={styles.exitbutton}>
-//                 <ExitButton onClick={() => {}}>Выйти</ExitButton>
-//             </div>
-//         </nav>
-//     );
-// };
-
-// export default NavAccount;
