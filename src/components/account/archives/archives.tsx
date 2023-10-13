205e25e94295c28a9e2ad9f04fa4481289c5ba49
Rustam <Rustamaaaa@bk.ru>
2023-10-13 15:59:24 +0300

    deleted index account file

--- src/components/account/archives/archives.tsx
+++ src/components/account/archives/archives.tsx
@@ -1,17 +1,10 @@
 "use client";
 
 import styles from "./archives.module.scss";
-// import { Button } from "@src/components/shared/ui/button/button";
 
 const Archives = () => {
     return (
         <div className={styles.ordersmain}>
-            {/* <article className={styles.noorders}>
-                <p className={styles.description}>
-                    У вас пока нет заказов. Но вы всегда можете исправить это, создав его
-                </p>
-                <Button onClick={() => {}}>Сделать заказ</Button>
-            </article> */}
             <article className={styles.order}>
                 <section className={styles.head}>
                     <p>Полка настенная</p>
--- src/components/account/my-orders/myorders.constants.tsx
+++
@@ -1,7 +0,0 @@
-// export default orderList = [
-//   {
-//     title='Полка',
-//     date='14/02/24',
-//     proposal='10000'
-//   },
-// ];
